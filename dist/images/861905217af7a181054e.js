fbf801e718400ed883782f01d2a756945814cb73
Евгений Белов <belov.e.e@mail.ru>
2024-11-08 18:55:05 +0300

    1

---
+++ dist/images/861905217af7a181054e.js
@@ -0,0 +1,1 @@
+import"./styles/normalize.css";import"swiper/css";import"swiper/css/navigation";import"swiper/css/pagination";import"./styles/main.scss";function openModal(){document.getElementById("modal").style.top="-80px"}function closeModal(){document.getElementById("modal").style.top="-4000px"}
