225341223ea83b196c4f7e231c25bdb175bfd65b
Евгений Белов <belov.e.e@mail.ru>
2024-11-10 15:52:35 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import './styles/normalize.css'
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import './styles/main.scss';
 
-// Menu
 
-function openModal() {
-    document.getElementById("modal").style.top = "-80px";
+var modal = document.getElementById('modal')
+var btnOpen = document.getElementById('btnBurger')
+var btnClose = document.getElementById('btnClose')
+
+btnOpen.onclick = function () {
+    modal.style.display = "block";
+}
+btnClose.onclick = function () {
+    modal.style.display = "none";
 }
 
-function closeModal() {
-    document.getElementById("modal").style.top = "-4000px";
+var modalForm = document.getElementById('modalForm')
+var formBntClose = document.getElementById('formBntClose')
+var formBntOpenBanner = document.getElementById('formBntOpenBanner')
+var formBntOpenFirst = document.getElementById('formBntOpenFirst')
+var formBntOpenSecond = document.getElementById('formBntOpenSecond')
+var formBntOpenThird = document.getElementById('formBntOpenThird')
+
+formBntOpenBanner.onclick = function () {
+    modalForm.style.top = "403px";
+}
+formBntOpenFirst.onclick = function () {
+    modalForm.style.top = "5031px";
+}
+formBntOpenSecond.onclick = function () {
+    modalForm.style.top = "5371px";
+}
+formBntOpenThird.onclick = function () {
+    modalForm.style.top = "5748px";
+}
+formBntClose.onclick = function () {
+    modalForm.style.top = "-4000px";
 }
