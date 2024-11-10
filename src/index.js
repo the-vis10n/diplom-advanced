8d72a75cd02485a611cbd4b24217a2d3457fca6d
Евгений Белов <belov.e.e@mail.ru>
2024-11-10 17:00:10 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ import './styles/main.scss';
 var modal = document.getElementById('modal')
 var btnOpen = document.getElementById('btnBurger')
 var btnClose = document.getElementById('btnClose')
+var container = document.getElementById('container')
+var header = document.getElementById('header')
+var backColor = document.getElementById('backColor')
 
 btnOpen.onclick = function () {
     modal.style.display = "block";
+    window.scrollTo (0,0);
+    container.style.filter = "blur(5px)";
+    header.style.filter = "blur(5px)"
+    backColor.style.filter = "blur(5px)"
+    
 }
 btnClose.onclick = function () {
     modal.style.display = "none";
+    container.style.filter = "none";
+    header.style.filter = "none"
+    backColor.style.filter = "none"
 }
 
 var modalForm = document.getElementById('modalForm')
@@ -21,17 +32,37 @@ var formBntOpenSecond = document.getElementById('formBntOpenSecond')
 var formBntOpenThird = document.getElementById('formBntOpenThird')
 
 formBntOpenBanner.onclick = function () {
-    modalForm.style.top = "403px";
+    modalForm.style.display = "block";
+    window.scrollTo (0,0);
+    container.style.filter = "blur(5px)";
+    header.style.filter = "blur(5px)"
+    backColor.style.filter = "blur(5px)"
 }
 formBntOpenFirst.onclick = function () {
-    modalForm.style.top = "5031px";
+    modalForm.style.display = "block";
+    window.scrollTo (0,0);
+    container.style.filter = "blur(5px)";
+    header.style.filter = "blur(5px)"
+    backColor.style.filter = "blur(5px)"
 }
 formBntOpenSecond.onclick = function () {
-    modalForm.style.top = "5371px";
+    modalForm.style.display = "block";
+    window.scrollTo (0,0);
+    container.style.filter = "blur(5px)";
+    header.style.filter = "blur(5px)"
+    backColor.style.filter = "blur(5px)"
 }
 formBntOpenThird.onclick = function () {
-    modalForm.style.top = "5748px";
+    modalForm.style.display = "block";
+    window.scrollTo (0,0);
+    container.style.filter = "blur(5px)";
+    header.style.filter = "blur(5px)"
+    backColor.style.filter = "blur(5px)"
 }
 formBntClose.onclick = function () {
-    modalForm.style.top = "-4000px";
+    modalForm.style.display = "none";
+    window.scrollTo (0,0);
+    container.style.filter = "none";
+    header.style.filter = "none"
+    backColor.style.filter = "none"
 }
